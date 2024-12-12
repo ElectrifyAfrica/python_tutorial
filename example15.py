ce2eef998dd007a5c8239f6a99a2c01b066e6d28
Daniel Aikhonmu <odaikhonmu@gmail.com>
2024-12-12 02:02:36 -0500

    2

--- example15.py
+++ example15.py
@@ -6,5 +6,26 @@ numbers.append(6)
 print(numbers)
 
 numbers = [1, 2, 3, 4, 5]
-numbers.append(6)
+numbers.insert(0, -1)
 print(numbers)
+
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(2, 2.5)
+print(numbers)
+
+numbers = [1, 1.5, 2.5, 3, 4, 5]
+numbers.remove(1.5)
+print(numbers)
+
+
+numbers = [1, 2, 3, 4, 5]
+print(1 in numbers)
+
+numbers = [1, 1.5, 2.5, 3, 4, 5]
+print(2.5 in numbers)
+
+numbers = [1, 1.5, 2.5, 3, 4, 5]
+print(2 in numbers)
+
+numbers = [1, 1.5, 2.5, 3, 4, 5]
+print(len(numbers))
---
+++ example16.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4, 5]
+for item in numbers:
+    print(item)
+
+
+numbers = [11, 12, 13, 14, 15]
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i = i + 1
