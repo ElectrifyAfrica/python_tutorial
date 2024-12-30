06328d4e0834fb960b83235efad047803b25881b
Daniel Aikhonmu <odaikhonmu@gmail.com>
2024-12-30 06:28:50 -0500

    Dec30b

---
+++ module_example2/app.py
@@ -0,0 +1,6 @@
+from utils import find_max
+
+numbers = [7, 6, 2, 19, 4, 10]
+maximum = find_max(numbers)
+
+print(maximum)
---
+++ module_example3/app.py
@@ -0,0 +1,7 @@
+from ecommerce import shipping           # Third Variant
+#from ecommerce.shipping import calc_shipping        #Second Variant
+#import ecommerce.shipping           #First variant
+
+shipping.calc_shipping()       #Third Variant
+#calc_shipping()         #Second Variant
+#ecommerce.shipping.calc_shipping()        # First Variant
